c21153d55dd673478df9eda517467b5c883e572f
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2024-11-14 22:32:19 +0000

    docs: Add examples to Track model

--- models/models.py
+++ models/models.py
@@ -37,3 +37,30 @@ class Track(BaseModel):
     cover_url: str = Field(default="", title="Cover URL")
 
     mood: TrackMood
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "name": "Believer",
+                    "artist_name": "Imagine Dragons",
+                    "artist_count": 1,
+                    "release_date": "2017-1-31",
+                    "in_playlists": 18986,
+                    "in_charts": 23,
+                    "streams": 2_594_040_133,
+                    "bpm": 125,
+                    "cover_url": "https://i.scdn.co/image/ab67616d0000b2735675e83f707f1d7271e5cf8a",
+                    "mood": {
+                        "danceability": 77,
+                        "valence": 74,
+                        "energy": 78,
+                        "acousticness": 4,
+                        "instrumentalness": 0,
+                        "liveness": 23,
+                        "speechiness": 11,
+                    },
+                }
+            ]
+        }
+    }
